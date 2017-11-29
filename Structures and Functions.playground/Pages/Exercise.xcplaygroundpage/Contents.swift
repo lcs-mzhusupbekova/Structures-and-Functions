dc9033c7eb58bc57ccfb099e985289ab708565a1
Zhusupbekova, May <mzhusupbekova@lcs-mba3239.lcs.on.ca>
2017-11-29 11:32:12 -0500

    got the perpendicular slope

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@ import Foundation
  
  The function header should look like this:
  
-  ![example](example1.png "example")
+ ![example](example1.png "example")
  
  You can re-use the **Point** structure and the *distance* function.
  
@@ -29,7 +29,48 @@ import Foundation
  Here is a [complete example showing how to find the shortest distance from a point to a line](http://russellgordon.ca/lcs/shortest-distance-example.pdf), using symbolic algebra.
  
  Your job is to implement a solution in code that carries out the necessary mathematics. The beautiful part of this effort is that, once you've "taught" the computer how to do this correctly, you will never need to do this type of work manually again. 👍🏽😅🎉
+ 
+ */
 
-*/
+// Begin here...
+//our given line is: y= -1/2x + 9.5
+// our given point is: (6,1,5)
 
-// Begin here... 
+//define a structure representing a point on a Cartesian plane
+struct Point {
+    var x : Double = 0.0
+    var y: Double = 0.0
+}
+// create an instance of the point from our problem
+var cabinSite = Point(x: 6, y: 1.5)
+
+// define a structure that represents a slope:
+struct Slope {
+    var rise : Double = 1.0
+    var run : Double = 1.0
+}
+
+
+// define a structure that represents a line
+struct Line {
+    var slope: Slope = Slope(rise: 1.0, run : 1.0)
+    var verticalIntercept : Double = 0.0
+}
+
+//create an instence of the line from our problem
+var slopeOfExistingRoad = Slope(rise: -1, run: 2)
+var existingRoad = Line(slope:slopeOfExistingRoad, verticalIntercept: 9.5)
+
+
+/// returns the slope of a line perpendicular to the provided line
+///
+/// - Parameter givenLine: the line we are starting from
+/// - Returns: the slope of the line that is perpendicular
+func getSlopeOfPerpendicularLine(from givenLine: Line) -> Slope {
+    givenLine.slope
+    return Slope(rise: givenLine.slope.run, run: givenLine.slope.rise * -1)
+}
+
+//get the perpendicular slope ( the slope of the new road from the cabin to the existing road)
+let perpendicularSlope = getSlopeOfPerpendicularLine(from: existingRoad)
+
